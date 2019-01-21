6544f556712d67713a1070bc7e3415672461dcec
liuzhehao <liuzhehao@zaonline.com>
2019-01-21 20:22:50 +0800

    restructure

---
+++ writesql/functions.py
@@ -0,0 +1,21 @@
+from writesql.utils import as_template_function, render_template
+
+
+def count(table_name, cols=None):
+    cols = cols or []
+    if isinstance(cols, str):
+        cols = [cols]
+    return render_template('count',
+                           table_name=table_name, cols=cols)
+
+
+def find_duplicates(table_name, cols, min_count=1):
+    if isinstance(cols, str):
+        cols = [cols]
+    return render_template('find_duplicates',
+                           table_name=table_name, cols=cols, min_count=min_count)
+
+
+if __name__ == '__main__':
+    # print(find_duplicates(table_name='table_name', cols=['cert_no', 'seq_no'], min_count=5))
+    print(count(table_name='some_table', cols=None))
